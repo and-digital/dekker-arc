63d039e2611cc753abaf0905ca87cd59fed01ab6
Namya <nraviprakash@and.digital>
2018-09-14 11:32:27 +0100

    Created server.js & app.js, added ESLint

--- routes/api/casestudy.route.js
+++
@@ -1,9 +0,0 @@
-var express = require('express')
-
-var router = express.Router()
-
-var CaseStudyController = require('../../controllers/casestudy.controller');
-
-router.get('/', CaseStudyController.getCaseStudies)
-
-module.exports = router;
